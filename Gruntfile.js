0a1433ddfc58405b5cadf4e42de6f1488b4fc782
Nicolas Ramz <nicolas.ramz@gmail.com>
2014-07-24 18:32:47 +0200

    FIXED: middleware example, see issue ##114

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,7 +161,7 @@ module.exports = function(grunt) {
             // inject a custom middleware into the array of default middlewares
             // this is likely the easiest way for other grunt plugins to
             // extend the behavior of grunt-contrib-connect
-            middlewares.push(function(req, res, next) {
+            middlewares.unshift(function(req, res, next) {
               if (req.url !== '/hello/world') {
                 return next();
               }
