e364363f422447210ac80bd419a68d671cdaeef1
Radko Dinev <radko.dinev@gmail.com>
2014-08-25 08:13:52 +0300

    First check code with jshint upon running npm/grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,6 +208,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-internal');
 
-  grunt.registerTask('test', ['connect', 'nodeunit']);
-  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
+  grunt.registerTask('test', ['jshint', 'connect', 'nodeunit']);
+  grunt.registerTask('default', ['test', 'build-contrib']);
 };
